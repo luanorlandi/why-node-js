07a7875811d1ddd2d622ff3daa207a67fedaa9ec
Luan Orlandi <orlandi.luan@gmail.com>
2018-08-15 01:44:51 -0300

    Stage 8 - socket.io

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const socket = require('socket.io');
 
 const port = 3000;
 const app = express();
-app.listen(port, console.log('Server up on port', port));
+const server = app.listen(port, console.log('Server up on port', port));
 
 app.get('/', function (request, response) {
   response.sendFile(__dirname + '/index.html');
 });
+
+const io = socket(server);
