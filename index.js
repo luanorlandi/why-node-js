5543bd0a3a2d29eb0370f1c76cdf50d14a87f03c
Luan Orlandi <orlandi.luan@gmail.com>
2018-08-17 02:11:09 -0300

    Update port with environment varible

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const socket = require('socket.io');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 const server = app.listen(port, console.log('Server up on port', port));
 
